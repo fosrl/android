579dd1b0642f918d93abbd1caae556b8f3648e51
Owen <owen@txv.io>
2026-07-07 21:18:13 -0400

    Update olm and versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "net.pangolin.Pangolin"
         minSdk = 24
         targetSdk = 35
-        versionCode = 20
-        versionName = "0.2.0"
+        versionCode = 21
+        versionName = "0.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
