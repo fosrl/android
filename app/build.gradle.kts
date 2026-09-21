6f0c34a39cf654f9d7ba609dab6888f6c5fe8c21
Owen <owen@txv.io>
2026-09-21 11:27:24 -0400

    Bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "net.pangolin.Pangolin"
         minSdk = 24
         targetSdk = 36
-        versionCode = 25
-        versionName = "0.6.0"
+        versionCode = 26
+        versionName = "0.7.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
